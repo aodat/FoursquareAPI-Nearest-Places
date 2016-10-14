e804d8e4514c3a6b42bb490af9876357e71ed225
souq <souq@souqs-macbook-pro-17.local>
2016-10-15 02:14:49 +0300

    - Add method to get current location - Add method to create rectangle of distance - Add permission message to user when open app to user geo location of device

--- FoursquareAPI Nearest Places/ViewController.swift
+++ FoursquareAPI Nearest Places/ViewController.swift
@@ -9,19 +9,41 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
 
+    var locationManager:CLLocationManager?
+    
+    // Distance covered in Meter.
+    let distanceCovered:Double = 400
+    
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
     }
-
     
-
+    // Get current location for user
+    override func viewDidAppear(_ animated: Bool)
+    {
+        if locationManager == nil {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            locationManager!.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+            locationManager!.requestAlwaysAuthorization()
+            locationManager!.distanceFilter = 50
+            locationManager!.startUpdatingLocation()
+        }
+    }
+    
+    // Creates a rectangular region of ( Distance coverd ) in meter
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if let mapView = self.mapView {
+            let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate, distanceCovered, distanceCovered)
+            mapView.setRegion(region, animated: true)
+        }
+    }
 }
 
