51d736103d4e2d0b01bf68ea7bc0874cb7b0d06b
souq <souq@souqs-macbook-pro-17.local>
2016-10-15 01:46:32 +0300

    - Building the UI with storyboard. - Adding The User GEO Location library to Project - Creating Outlets for tableView and mapView

--- FoursquareAPI Nearest Places/ViewController.swift
+++ FoursquareAPI Nearest Places/ViewController.swift
@@ -7,19 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
