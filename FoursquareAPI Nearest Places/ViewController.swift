48eec165a6af7738c34aa49d119ae7eaeadc8c7a
souq <souq@souqs-macbook-pro-17.local>
2016-10-15 16:04:01 +0300

    - Adding Search bar on top of view, to search for specific location type

--- FoursquareAPI Nearest Places/ViewController.swift
+++ FoursquareAPI Nearest Places/ViewController.swift
@@ -24,11 +24,11 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     let clientSecret    = "SNMRVYAOMS421JEMERMRIKBKRC0MMZWGF1CDF3AN1JCDPGIV"
     var lattidue        = ""
     var longitude       = ""
-    let searchQuery     = "cafe"
+    var searchQuery     = ""
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var searchView: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,6 +73,8 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         }
     }
     
+    // Adding pin to map view with params 
+    // Name of pin, Lat and Lng of location
     func addPin(name: String, lat: Double, lng:Double){
         let annotation = MKPointAnnotation()
             annotation.coordinate.latitude = lat
@@ -81,18 +83,34 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
             mapView.addAnnotation(annotation)
     }
     
+    // Search query for specific places type
+    // Get new data from server based on search query
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if searchBar.text != nil {
+            searchQuery = searchBar.text!
+            getData()
+            self.view.endEditing(true)
+        }
+    }
     
     // Alamofire request to get data from server and parse it via Swifty Json
     func getData(){
-        
+
         // Server request with params
         let server = "https://api.foursquare.com/v2/venues/search?client_id=\(clientID)&client_secret=\(clientSecret)&v=20130815&query=\(searchQuery)&ll=\(lattidue),\(longitude)"
-        
+
         Alamofire.request(.GET, server).responseJSON { (response) -> Void in
             if((response.result.value) != nil) {
                 let jsonResponseValue = JSON(response.result.value!)
                 if let restaurentArray = jsonResponseValue["response"]["venues"].array {
+                    // Removing all old elements in array
+                    // Removing all old pins on map
                     self.restaurantsList.removeAll()
+                    let allAnnotations = self.mapView.annotations
+                    self.mapView.removeAnnotations(allAnnotations)
+                    
+                    // Adding new elements to array
+                    // Adding new pins to map
                     for restDictinary in restaurentArray {
                         let restaurent = Restaurent(json: restDictinary)
                         self.restaurantsList.append(restaurent)
